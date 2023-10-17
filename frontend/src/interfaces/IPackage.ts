4a2943b82b5b9f3467762944bb3dc8f8ec346816
warakorn1799 <warakorn1799@gmail.com>
2023-10-17 20:16:05 +0700

    เเก้ BUG!!!!

--- frontend/src/interfaces/IPackage.ts
+++ frontend/src/interfaces/IPackage.ts
@@ -3,15 +3,9 @@ import { PromotionInterface } from "./IPromotion";
 export interface PackageInterface {
     ID?: number; 
     Name?: string;
-    Type?: string;
-    Fromdate?: Date;
-    Todate?: Date;
-    Day?: string;
-    Status?: string;
-    Person?: number;
     Detail?: string;
-    Price?: number;
     Priceadult?: number;
     Pricechil?: number;
     PromotionID?: PromotionInterface;
+    Highlights?: string;
 }
